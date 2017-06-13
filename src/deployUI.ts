92dd1b4ac30b53a7efc8dddde56827c279537061
Parvez <parvezp@microsoft.com>
2017-06-13 12:37:34 -0700

    First checkin for the remote monitoring cli (#1)
    Pre-req
    - Login through https://aka.ms/devicelogin and code prompt
    
    Data needed to create template deployment
     - Get solution/resourceGroup name
     - Get user name and pwd if required
    - Get the local arm template
    - Create parameters json from options so far
    - Subscriptions list from either DeviceTokenCredentials or SubscriptionsManagementClient
    - Location information(currently hard coded)
    
    Actions on data collected
    - Create resource group
    - Submit deployment

---
+++ src/deployUI.ts
@@ -0,0 +1,36 @@
+import * as inquirer from "inquirer";
+
+class DeployUI {
+    private deploying = 'Deploying...'
+    private deployed = 'Deployed successfully'
+    private loader = [
+        '/ ' + this.deploying,
+        '| ' + this.deploying,
+        '\\ ' + this.deploying,
+        '- ' + this.deploying
+        ];
+
+    private i = 4;
+    private ui = new inquirer.ui.BottomBar();
+
+    private id: NodeJS.Timer;
+    
+    start(): void {
+        let _this = this
+        this.id = setInterval(function () {
+            _this.ui.updateBottomBar(_this.loader[_this.i++ % 4]);
+        }, 200);
+    }
+
+    stop(err?: Error): void {
+        clearInterval(this.id);
+        let message = this.deployed;
+        if(err){
+            message = 'Deployment failed' + err;
+        }
+
+        this.ui.updateBottomBar(message);
+    }
+}
+
+export default DeployUI;
