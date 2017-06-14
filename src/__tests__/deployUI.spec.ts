5b757869fdccc4b4339317ba381edfcdf2ffc88f
Parvez <ppathan@users.noreply.github.com>
2017-06-14 11:54:34 -0700

    Adding tslint and jest tests
    -Linting the source files
    -Included jest tests(WIP)

---
+++ src/__tests__/deployUI.spec.ts
@@ -0,0 +1,7 @@
+import DeployUI from '../deployUI';
+import { expect } from 'jest';
+
+test('adds 1 + 2 to equal 3', () => {
+  const deployUI = new DeployUI();
+  expect(deployUI).toBe();
+});
--- src/deployUI.ts
+++ src/deployUI.ts
@@ -1,31 +1,32 @@
-import * as inquirer from "inquirer";
+import * as inquirer from 'inquirer';
 
 class DeployUI {
-    private deploying = 'Deploying...'
-    private deployed = 'Deployed successfully'
+    private deploying = 'Deploying...';
+    private deployed = 'Deployed successfully';
     private loader = [
         '/ ' + this.deploying,
         '| ' + this.deploying,
         '\\ ' + this.deploying,
-        '- ' + this.deploying
+        '- ' + this.deploying,
         ];
 
     private i = 4;
     private ui = new inquirer.ui.BottomBar();
 
     private id: NodeJS.Timer;
-    
-    start(): void {
-        let _this = this
-        this.id = setInterval(function () {
-            _this.ui.updateBottomBar(_this.loader[_this.i++ % 4]);
-        }, 200);
+
+    public start(): void {
+        this.id = setInterval(
+            () => {
+                this.ui.updateBottomBar(this.loader[this.i++ % 4]);
+            }, 
+            200);
     }
 
-    stop(err?: Error): void {
+    public stop(err?: Error): void {
         clearInterval(this.id);
         let message = this.deployed;
-        if(err){
+        if (err) {
             message = 'Deployment failed ' + err;
         }
 
