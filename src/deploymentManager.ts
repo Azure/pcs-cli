f4aa0b505f110d9f11eda248e10e4d5ef4fd45f8
Parvez Pathan <parvezp@microsoft.com>
2017-06-16 14:36:27 -0700

    Added remote monitoring template
    -Added remote monitoring template which deploys
     - DocDB
     - Iothub
     - Websites
    -Made remotemonitoring default type and added locations list
    -Some refactoring to make code easy to read

---
+++ src/deploymentManager.ts
@@ -0,0 +1,63 @@
+import * as ResourceManagement from 'azure-arm-resource';
+import * as msRestAzure from 'ms-rest-azure';
+
+import { Answers, Question } from 'inquirer';
+import DeployUI from './deployUI';
+
+type ResourceGroup = ResourceManagement.ResourceModels.ResourceGroup;
+type Deployment = ResourceManagement.ResourceModels.Deployment;
+type DeploymentProperties = ResourceManagement.ResourceModels.DeploymentProperties;
+
+export interface IDeploymentManager {
+    submit(solutionName: string, subscriptionName: string, location: string): Promise<any>;
+}
+
+export class DeploymentManager implements IDeploymentManager {
+    private _authReponse: msRestAzure.AuthResponse;
+    private _solutionType: string;
+    private _template: any;
+    private _parameters: any;
+
+    constructor(authResonse: msRestAzure.AuthResponse, solutionType: string, template: any, parameters: any) {
+        this._authReponse = authResonse;
+        this._solutionType = solutionType;
+        this._template = template;
+        this._parameters = parameters;
+    }
+
+    public submit(solutionName: string, subscriptionName: string, location: string): Promise<any> {
+            const selectedSubscription: msRestAzure.LinkedSubscription[] =
+                this._authReponse.subscriptions.filter((linkedSubs: msRestAzure.LinkedSubscription) => {
+                    if (linkedSubs.name === subscriptionName) {
+                        return linkedSubs.id;
+                    }
+                });
+            const client = new ResourceManagement
+                .ResourceManagementClient(this._authReponse.credentials, selectedSubscription[0].id);
+            const resourceGroup: ResourceGroup = {
+                location,                
+                tags: { IotSuiteType: this._solutionType },
+            };
+
+            this._parameters.solutionName.value = solutionName;
+            const properties: DeploymentProperties = {
+                mode: 'Incremental',
+                parameters: this._parameters,
+                template: this._template,
+            };
+
+            const deployment: Deployment = { properties };
+            const deployUI = new DeployUI();
+            return client.resourceGroups.createOrUpdate('rg-' + solutionName, resourceGroup)
+                .then((result: ResourceGroup) => {
+                    deployUI.start();
+                    return client.deployments
+                    .createOrUpdate(result.name as string, 'deployment-' + solutionName, deployment)
+                    .then(() => {
+                        deployUI.stop();
+                    });
+                }).catch((err: Error) => {
+                    deployUI.stop(err);
+                });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,47 @@
 #!/usr/bin/env node
 
-import * as ResourceManagement from 'azure-arm-resource';
 import * as chalk from 'chalk';
 import * as fs from 'fs';
 import * as inquirer from 'inquirer';
-import * as msRest from 'ms-rest';
 import * as msRestAzure from 'ms-rest-azure';
 
-import { Answers, Question, Questions } from 'inquirer';
-import { BlobService, TableService } from 'azure';
-
+import { Answers, Question } from 'inquirer';
+import { IDeploymentManager, DeploymentManager } from './DeploymentManager';
+import { IQuestions, Questions } from './Questions';
 import { Command } from 'commander';
-import DeployUI from './deployUI';
 
 const packageJson = require('../package.json');
 
-type ResourceGroup = ResourceManagement.ResourceModels.ResourceGroup;
-type Deployment = ResourceManagement.ResourceModels.Deployment;
-type DeploymentProperties = ResourceManagement.ResourceModels.DeploymentProperties;
-type DeviceTokenCredentials = msRestAzure.DeviceTokenCredentials;
-
-const solutionNameRegex: RegExp = /^[a-z0-9]{1,17}$/;
-const userNameRegex: RegExp = /^[a-zA-Z_][a-zA-Z0-9_@$#]{0,127}$/;
-
-/* tslint:disable */
-const passwordRegex: RegExp = /^(?!.*')((?=.*[a-z])(?=.*[0-9])(?=.*\W)|(?=.*[A-Z])(?=.*[0-9])(?=.*\W)|(?=.*[A-Z])(?=.*[a-z])(?=.*\W)|(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])|(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\W)).{8,128}$/;
-/* tslint:enable */
+let solutionType: string = 'RemoteMonitoring';
+let template = require('../templates/remoteMonitoring.json');
+let parameters = require('../templates/remoteMonitoringParameters.json');
 
 const program = new Command(packageJson.name)
     .version(packageJson.version, '-v, --version')
-    .usage(`${chalk.green('<login>')} [options]`)
+    .usage(`${chalk.green('<solutiontype>')} [options]`)
+    .action((type)  => {
+        if  (type) {
+            solutionType = type;
+            const solution = type + '.json';
+            const params = type + 'Parameters.json';
+            template = require('../templates/' + solution);
+            parameters = require('../templates/' + params);
+        }
+    })
+    .on('--help', () => {
+        console.log(
+            `    Default value for ${chalk.green('<solutiontype>')} is ${chalk.green('RemoteMonitoring')}.`
+            );
+        console.log();
+        console.log(
+            `    If you have any problems, do not hesitate to file an issue:`
+            );
+        console.log(
+            `    ${chalk.cyan('https://github.com/azure/azure-remote-monitoring-cli/issues/new')}`
+            );
+        console.log();
+    })
     .parse(process.argv);
-
-const questions: Question[] = [
-    {
-        message: 'Enter a solution name:',
-        name: 'solutionName',
-        type: 'input',
-        validate: (value: string) => {
-            const pass: RegExpMatchArray | null = value.match(solutionNameRegex);
-            if (pass) {
-                return true;
-            }
-
-            return 'Please enter a valid solution name';
-        },
-    },
-    {
-        message: 'Enter a sql server username:',
-        name: 'userName',
-        type: 'input',
-        validate: (value: string) => {
-            const pass: RegExpMatchArray | null = value.match(userNameRegex);
-            if (pass) {
-                return true;
-            }
-
-            return 'Please enter a valid user name';
-        },
-    },
-    {
-        message: 'Enter a password:',
-        name: 'sqlPassword',
-        type: 'password',
-        validate: (value: string) => {
-            const pass: RegExpMatchArray | null = value.match(passwordRegex);
-            if (pass) {
-                return true;
-            }
-
-            return 'Please enter a valid user name';
-        },
-        // ,
-        // TODO:
-        // Currently the mask option is not available in the inquirer types.
-        // I have submitted a PR and once it is release we can enable mask
-        // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/17094#issuecomment-307501936
-        // mask: '*'
-        //
-    },
-    {
-        message: 'Enter a template file path:',
-        name: 'templateFilePath',
-        type: 'input',
-    },
-    {
-        message: 'Enter a parameter file path:',
-        name: 'parametersFilePath',
-        type: 'input',
-    }];
 
 main();
 
@@ -112,72 +65,35 @@ function main() {
      */
     msRestAzure.interactiveLoginWithAuthResponse().then((authResponse: msRestAzure.AuthResponse) => {
         const subs: string[] = [];
+        const deploymentManager: IDeploymentManager = new DeploymentManager(authResponse, solutionType, template, parameters);
 
         authResponse.subscriptions.map((subscription: msRestAzure.LinkedSubscription) => {
             subs.push(subscription.name);
         });
-        questions.push(
+
+        const questions: IQuestions = new Questions();
+        questions.addQuestions([
         {
             choices: subs,
             message: 'Select a subscription:',
-            name: 'subscriptionsList',
+            name: 'subscription',
             type: 'list',
         },
         {
             // TODO: List the locations based on selected subscription
-            choices: ['westus', 'eastus'],
+            choices: ['East US', 'North Europe', 'East Asia', 'West US', 'West Europe', 'Southeast Asia', 
+                     'Japan East', 'Japan West', 'Australia East', 'Australia Southeast'],
             message: 'Select a location',
             name: 'location',
             type: 'list',
-        });
+        }]);
 
-        inquirer.prompt(questions).then((answers: Answers) => {
-            const selectedSubscription: msRestAzure.LinkedSubscription[] =
-                authResponse.subscriptions.filter((linkedSubs: msRestAzure.LinkedSubscription) => {
-                    if (linkedSubs.name === answers.subscriptionsList) {
-                        return linkedSubs.id;
-                    }
-                });
-            const client = new ResourceManagement
-                .ResourceManagementClient(authResponse.credentials, selectedSubscription[0].id);
-            const resourceGroup: ResourceGroup = {
-                location: answers.location,
-                name: answers.name,
-                tags: { pcs: 'Pre-configured solution' },
-            };
-
-            let template: any;
-            let parameters: any;
-
-            try {
-                const filePath: string = answers.templateFilePath;
-                const buffer: Buffer = fs.readFileSync(filePath);
-                template = JSON.parse(buffer.toString());
-                parameters = JSON.parse(fs.readFileSync(answers.parametersFilePath).toString());
-                parameters.sqlAdministratorLogin.value = answers.userName;
-                parameters.sqlAdministratorLoginPassword.value = answers.sqlPassword;
-            } catch (error) {
-                console.log(error);
-            }
-
-            const properties: DeploymentProperties = {
-                mode: 'Incremental',
-                parameters,
-                template,
-            };
-            const deployment: Deployment = { properties };
-            const deployUI = new DeployUI();
-            client.resourceGroups.createOrUpdate(answers.solutionName, resourceGroup)
-                .then((result: ResourceGroup) => {
-                    deployUI.start();
-                    return client.deployments
-                    .createOrUpdate(result.name as string, answers.solutionName + '-deployment', deployment)
-                    .then(() => {
-                        deployUI.stop();
-                    });
-                }).catch((err: Error) => {
-                    deployUI.stop(err);
-                });
+        inquirer.prompt(questions.value)
+        .then((answers: Answers) => {
+            return deploymentManager.submit(answers.solutionName, answers.subscription, answers.location);
+        })
+        .catch((error: Error) => {
+            console.log('Prompt error: ' + error);
         });
     });
 }
