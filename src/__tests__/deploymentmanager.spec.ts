d2132dc8e50da69351346c72a24d8b35913c6b0f
Parvez Pathan <parvezp@microsoft.com>
2017-06-20 19:50:10 -0700

    Added test to verify empty parameter returns rejection

---
+++ src/__tests__/deploymentmanager.spec.ts
@@ -0,0 +1,23 @@
+import * as msRestAzure from 'ms-rest-azure';
+import { DeploymentManager, IDeploymentManager } from '../DeploymentManager';
+
+const solutionType: string = 'RemoteMonitoring';
+
+describe('Template deployment through DeploymentManager', () => {
+    let deploymentManager: IDeploymentManager;
+    beforeAll(() => {
+        const authResponse: msRestAzure.AuthResponse = { 
+            credentials: new msRestAzure.DeviceTokenCredentials(),
+            subscriptions: []
+        };
+        deploymentManager = new DeploymentManager(authResponse, solutionType, null, null);
+    });
+
+    test('Empty solution, subscription or location should fail', () => {
+        return deploymentManager
+        .submit('', '' , '')
+        .catch((error) => { 
+            expect(error).toBeDefined();
+        });
+    });
+});
