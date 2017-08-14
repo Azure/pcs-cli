f9fa30fe82fde9ef411a49f2c145858ed3512302
Parvez Pathan <parvezp@microsoft.com>
2017-08-14 15:14:31 -0700

    Enabling demo deployment for the PCS

--- src/questions.ts
+++ src/questions.ts
@@ -9,10 +9,35 @@ export interface IQuestions {
 }
 
 export class Questions implements Questions {
+    /* User name requirements: https://docs.microsoft.com/en-us/azure/virtual-machines/windows/faq
+                               #what-are-the-username-requirements-when-creating-a-vm
+       Usernames can be a maximum of 20 characters in length and cannot end in a period ('.').
+    */
+    public static userNameRegex: RegExp = /^(.(?!\.$)){1,20}$/;
+    public static notAllowedUserNames = [ 'administrator', 'admin', 'user', 'user1',
+                                          'test', 'user2', 'test1', 'user3',
+                                          'admin1', '1', '123', 'a',
+                                          'actuser', 'adm', 'admin2', 'aspnet',
+                                          'backup', 'console', 'david', 'guest',
+                                          'john', 'owner', 'root', 'server', 'sql',
+                                          'support', 'support_388945a0', 'sys',
+                                          'test2', 'test3', 'user4', 'user5' ];
+    /* Password requirements: https://docs.microsoft.com/en-us/azure/virtual-machines/windows/faq
+                              #what-are-the-password-requirements-when-creating-a-vm
+       Passwords must be 12 - 123 characters in length and meet 3 out of the following 4 complexity requirements:
+       Have lower characters
+       Have upper characters
+       Have a digit
+       Have a special character (Regex match [\W_])
+    */
+// tslint:disable
+    public static passwordRegex: RegExp = /^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?\d)(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?\d)(?=.*?[^a-zA-Z0-9])).{12,14}$/;
+// tslint:enable
+    public static notAllowedPasswords = ['abc@123', 'P@$$w0rd', '@ssw0rd', 'P@ssword123', 'Pa$$word',
+                                        'pass@word1', 'Password!', 'Password1', 'Password22', 'iloveyou!'];
     public solutionNameRegex: RegExp = /^[a-z0-9]{1,17}$/;
-
     public locations: string[] = ['East US', 'North Europe', 'East Asia', 'West US', 'West Europe', 'Southeast Asia', 
-                     'Japan East', 'Japan West', 'Australia East', 'Australia Southeast'];
+                     'Japan East', 'Japan West', 'Australia East', 'Australia Southeast'];  
 
     private _questions: Question[] ;
 
