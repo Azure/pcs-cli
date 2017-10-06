379429e6d2e3f048d0e78b7e94447ee1be98e50b
Devis Lucato <devis@lucato.it>
2017-10-05 21:44:52 -0700

    Update simulate.sh to bypass auth

--- remotemonitoring/single-vm/scripts/simulate.sh
+++ remotemonitoring/single-vm/scripts/simulate.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -e
 
-# TODO: the script doesn't work due to Auth
-#       https://github.com/Azure/device-simulation-dotnet/issues/66
-
 cd /app
 
-echo "Script temporarily disabled, see https://github.com/Azure/device-simulation-dotnet/issues/66"
-exit -1
-
 echo "Starting simulation..."
-ISUP=$(curl -sk https://localhost/devicesimulation/v1/status | grep "Alive" | wc -l)
+ISUP=$(docker exec -it app_devicesimulation_1 curl -sk http://localhost:9003/v1/status | grep "Alive" | wc -l)
 while [[ "$ISUP" == "0" ]]; do
   echo "Waiting for simulation service to be available..."
   sleep 4
-  ISUP=$(curl -sk https://localhost/devicesimulation/v1/status | grep "Alive" | wc -l)
+  ISUP=$(docker exec -it app_devicesimulation_1 curl -sk http://localhost:9003/v1/status |grep "Alive" | wc -l)
 done
-curl -sk -X POST "https://localhost/devicesimulation/v1/simulations?template=default" -H "content-type: application/json" -d "{}"
+docker exec -it app_devicesimulation_1 curl -sk -X POST "http://localhost:9003/v1/simulations?template=default" -H "content-type: application/json" -d "{}"
 echo
