498a1c558b7e6539842a36cfef73675c2a30c789
Devis Lucato <devis@lucato.it>
2017-11-09 23:25:00 -0800

    new ARM templates for device simulation

---
+++ solutions/devicesimulation-nohub/single-vm/setup.sh
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+# Copyright (c) Microsoft. All rights reserved.
+# Note: Windows Bash doesn't support shebang extra params
+set -ex
+
+APP_PATH="/app"
+WEBUICONFIG="${APP_PATH}/webui-config.js"
+WEBUICONFIG_SAFE="${APP_PATH}/webui-config.js.safe"
+WEBUICONFIG_UNSAFE="${APP_PATH}/webui-config.js.unsafe"
+ENVVARS="${APP_PATH}/env-vars"
+DOCKERCOMPOSE="${APP_PATH}/docker-compose.yml"
+CERTS="${APP_PATH}/certs"
+CERT="${CERTS}/tls.crt"
+PKEY="${CERTS}/tls.key"
+
+REPOSITORY="https://raw.githubusercontent.com/Azure/pcs-cli/azure-iot-pcs-simulation/devicesimulation/single-vm"
+SCRIPTS_URL="${REPOSITORY}/scripts/"
+
+# ========================================================================
+
+export HOST_NAME="${1:-localhost}"
+export PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING="${3}"
+export PCS_CERTIFICATE="${4}"
+export PCS_CERTIFICATE_KEY="${5}"
+export PCS_AUTH_AUDIENCE="${6}"
+export PCS_AUTH_ISSUER="https://sts.windows.net/${7}/"
+export PCS_WEBUI_AUTH_TYPE="aad"
+export PCS_WEBUI_AUTH_AAD_APPID="${6}"
+export PCS_WEBUI_AUTH_AAD_TENANT="${7}"
+export PCS_WEBUI_AUTH_AAD_INSTANCE="${8}"
+
+# ========================================================================
+
+# Configure Docker registry based on host name
+# ToDo: we may need to add similar parameter to AzureGermanCloud and AzureUSGovernment
+config_for_azure_china() {
+    set +e
+    local host_name=$1
+    if (echo $host_name | grep -c  "\.cn$") ; then
+        # If the host name has .cn suffix, dockerhub in China will be used to avoid slow network traffic failure.
+        local config_file='/etc/docker/daemon.json'
+        echo "{\"registry-mirrors\": [\"https://registry.docker-cn.com\"]}" > ${config_file}
+        service docker restart
+
+        # Rewrite the AAD issuer in Azure China environment
+        export PCS_AUTH_ISSUER="https://sts.chinacloudapi.cn/$2/"
+    fi
+    set -e
+}
+
+config_for_azure_china $HOST_NAME $5
+
+# ========================================================================
+
+mkdir -p ${APP_PATH}
+cd ${APP_PATH}
+
+# ========================================================================
+
+# Docker compose file
+
+DOCKERCOMPOSE_SOURCE="${REPOSITORY}/docker-compose.yml"
+wget $DOCKERCOMPOSE_SOURCE -O ${DOCKERCOMPOSE}
+
+# ========================================================================
+
+# HTTPS certificates
+mkdir -p ${CERTS}
+touch ${CERT} && chmod 550 ${CERT}
+touch ${PKEY} && chmod 550 ${PKEY}
+# Always have quotes around the certificate and key value to preserve the formatting
+echo "${PCS_CERTIFICATE}"      > ${CERT}
+echo "${PCS_CERTIFICATE_KEY}"  > ${PKEY}
+
+# ========================================================================
+
+# Download scripts
+wget $SCRIPTS_URL/logs.sh     -O /app/logs.sh     && chmod 750 /app/logs.sh
+wget $SCRIPTS_URL/start.sh    -O /app/start.sh    && chmod 750 /app/start.sh
+wget $SCRIPTS_URL/stats.sh    -O /app/stats.sh    && chmod 750 /app/stats.sh
+wget $SCRIPTS_URL/status.sh   -O /app/status.sh   && chmod 750 /app/status.sh
+wget $SCRIPTS_URL/stop.sh     -O /app/stop.sh     && chmod 750 /app/stop.sh
+
+# ========================================================================
+
+# Web App configuration
+touch ${WEBUICONFIG} && chmod 444 ${WEBUICONFIG}
+touch ${WEBUICONFIG_SAFE} && chmod 444 ${WEBUICONFIG_SAFE}
+touch ${WEBUICONFIG_UNSAFE} && chmod 444 ${WEBUICONFIG_UNSAFE}
+
+echo "var DeploymentConfig = {"                       >> ${WEBUICONFIG_SAFE}
+echo "  authEnabled: true,"                           >> ${WEBUICONFIG_SAFE}
+echo "  authType: '${PCS_WEBUI_AUTH_TYPE}',"          >> ${WEBUICONFIG_SAFE}
+echo "  aad : {"                                      >> ${WEBUICONFIG_SAFE}
+echo "    tenant: '${PCS_WEBUI_AUTH_AAD_TENANT}',"    >> ${WEBUICONFIG_SAFE}
+echo "    appId: '${PCS_WEBUI_AUTH_AAD_APPID}',"      >> ${WEBUICONFIG_SAFE}
+echo "    instance: '${PCS_WEBUI_AUTH_AAD_INSTANCE}'" >> ${WEBUICONFIG_SAFE}
+echo "  }"                                            >> ${WEBUICONFIG_SAFE}
+echo "}"                                              >> ${WEBUICONFIG_SAFE}
+
+echo "var DeploymentConfig = {"                       >> ${WEBUICONFIG_UNSAFE}
+echo "  authEnabled: false,"                          >> ${WEBUICONFIG_UNSAFE}
+echo "  authType: '${PCS_WEBUI_AUTH_TYPE}',"          >> ${WEBUICONFIG_UNSAFE}
+echo "  aad : {"                                      >> ${WEBUICONFIG_UNSAFE}
+echo "    tenant: '${PCS_WEBUI_AUTH_AAD_TENANT}',"    >> ${WEBUICONFIG_UNSAFE}
+echo "    appId: '${PCS_WEBUI_AUTH_AAD_APPID}',"      >> ${WEBUICONFIG_UNSAFE}
+echo "    instance: '${PCS_WEBUI_AUTH_AAD_INSTANCE}'" >> ${WEBUICONFIG_UNSAFE}
+echo "  }"                                            >> ${WEBUICONFIG_UNSAFE}
+echo "}"                                              >> ${WEBUICONFIG_UNSAFE}
+
+cp -p ${WEBUICONFIG_SAFE} ${WEBUICONFIG}
+
+# ========================================================================
+
+# Environment variables
+touch ${ENVVARS} && chmod 440 ${ENVVARS}
+
+echo "export HOST_NAME=\"${HOST_NAME}\""                                                                 >> ${ENVVARS}
+echo "export PCS_AUTH_ISSUER=\"${PCS_AUTH_ISSUER}\""                                                     >> ${ENVVARS}
+echo "export PCS_AUTH_AUDIENCE=\"${PCS_AUTH_AUDIENCE}\""                                                 >> ${ENVVARS}
+echo "export PCS_AUTH_AAD_GLOBAL_TENANTID=\"${PCS_AUTH_AAD_GLOBAL_TENANTID}\""                           >> ${ENVVARS}
+echo "export PCS_AUTH_AAD_GLOBAL_CLIENTID=\"${PCS_AUTH_AAD_GLOBAL_CLIENTID}\""                           >> ${ENVVARS}
+echo "export PCS_AUTH_AAD_GLOBAL_LOGINURI=\"${PCS_AUTH_AAD_GLOBAL_LOGINURI}\""                           >> ${ENVVARS}
+echo "export PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING=\"${PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING}\""   >> ${ENVVARS}
+echo ""                                                                                                  >> ${ENVVARS}
+echo "##########################################################################################"        >> ${ENVVARS}
+echo "# Development settings, don't change these in Production"                                          >> ${ENVVARS}
+echo "# You can run 'start.sh --unsafe' to temporarily disable Auth and Cross-Origin protections"        >> ${ENVVARS}
+echo ""                                                                                                  >> ${ENVVARS}
+echo "# Format: true | false"                                                                            >> ${ENVVARS}
+echo "# empty => Auth required"                                                                          >> ${ENVVARS}
+echo "export PCS_AUTH_REQUIRED=\"\""                                                                     >> ${ENVVARS}
+echo ""                                                                                                  >> ${ENVVARS}
+echo "# Format: { 'origins': ['*'], 'methods': ['*'], 'headers': ['*'] }"                                >> ${ENVVARS}
+echo "# empty => CORS support disabled"                                                                  >> ${ENVVARS}
+echo "export PCS_CORS_WHITELIST=\"\""                                                                    >> ${ENVVARS}
+
+# ========================================================================
+
+nohup /app/start.sh &
