50452c4174472fb79b230bf73de3efbef42d29ee
Hua Zhang <huazhan@microsoft.com>
2018-03-29 19:07:05 -0700

    Remove the code to disable PasswordAuthentication

--- solutions/remotemonitoring/single-vm/setup.sh
+++ solutions/remotemonitoring/single-vm/setup.sh
@@ -88,8 +88,6 @@ config_ssh() {
     local config_path="${1:-/etc/ssh/sshd_config}"
     switch_off 'HostKey' '/etc/ssh/ssh_host_dsa_key' $config_path
     switch_off 'HostKey' '/etc/ssh/ssh_host_ecdsa_key' $config_path
-    # TODO: Change to no to disable tunnelled clear text passwords
-    # switch_on 'PasswordAuthentication' 'no' $config_path
     switch_on 'KexAlgorithms' 'curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256' $config_path
     switch_on 'Ciphers' 'chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr' $config_path
     switch_on 'MACs' 'hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com' $config_path
