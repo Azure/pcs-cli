bf210296e24c6f6e1763c52933491913c2720cc0
Parvez <ppathan@users.noreply.github.com>
2018-04-27 15:26:28 -0700

    Removing telemetry-agent from the deployment (#274)
    * Removing telemetry-agent from the deployment
    
    * Updating message to show wait for ASA job

--- src/deploymentmanager.ts
+++ src/deploymentmanager.ts
@@ -220,8 +220,7 @@ export class DeploymentManager implements IDeploymentManager {
             .then(() => {
                 // wait for streaming jobs to start if it is included in template and sku is not local
                 if (deploymentProperties.outputs.streamingJobsName && answers.deploymentSku !== 'local') {
-                    deployUI.start(`Waiting for streaming jobs to be started, this could take up to a few minutes.`);
-                    deployUI.start(`${deploymentProperties.outputs.streamingJobsName.value}`);
+                    deployUI.start(`Waiting for streaming jobs to be started, this could take up to few minutes.`);
                     return this.waitForStreamingJobsToStart(answers.solutionName, deploymentProperties.outputs.streamingJobsName.value);
                 }
                 return Promise.resolve(true);
