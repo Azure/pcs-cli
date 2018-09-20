865c764cd1e4c3f16f567f2c4595c8b54cddb303
Avni Patel <apatel@microsoft.com>
2018-09-20 15:05:45 -0700

    add back hub

--- solutions/devicesimulation/single-vm/setup.sh
+++ solutions/devicesimulation/single-vm/setup.sh
@@ -31,6 +31,11 @@ while [ "$#" -gt 0 ]; do
         --solution-type)           PCS_SOLUTION_TYPE="$2" ;;
         --solution-name)           PCS_SOLUTION_NAME="$2" ;;
         --resource-group)          PCS_RESOURCE_GROUP="$2" ;;
+        --iothub-name)             PCS_IOHUB_NAME="$2" ;;	
+        --iothub-sku)              PCS_IOTHUB_SKU="$2" ;;	
+        --iothub-tier)             PCS_IOTHUB_TIER="$2" ;;	
+        --iothub-units)            PCS_IOTHUB_UNITS="$2" ;;	
+        --iothub-connstring)       PCS_IOTHUB_CONNSTRING="$2" ;;
         --docdb-name)              PCS_DOCDB_NAME="$2" ;;
         --docdb-connstring)        PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING="$2" ;;
         --storage-connstring)      PCS_AZURE_STORAGE_ACCOUNT="$2" ;;
@@ -196,6 +201,10 @@ echo "export PCS_SEED_TEMPLATE=\"device-simulation-template\""                  
 echo "export PCS_CLOUD_TYPE=\"${PCS_CLOUD_TYPE}\""                                                       >> ${ENVVARS}
 echo "export PCS_DEPLOYMENT_ID=\"${PCS_DEPLOYMENT_ID}\""                                                 >> ${ENVVARS}
 echo "export PCS_DIAGNOSTICS_ENDPOINT_URL=\"${PCS_DIAGNOSTICS_ENDPOINT_URL}\""                           >> ${ENVVARS}
+echo "export PCS_DEVICESIMULATION_WEBSERVICE_URL=\"\""                                                   >> ${ENVVARS}	
+echo "export PCS_TELEMETRY_WEBSERVICE_URL=\"\""                                                          >> ${ENVVARS}	
+echo "export PCS_IOTHUBMANAGER_WEBSERVICE_URL=\"\""                                                      >> ${ENVVARS}	
+echo "export PCS_BINGMAP_KEY=\"\""                                                                       >> ${ENVVARS}
 echo "export PCS_AZURE_STORAGE_ACCOUNT=\"${PCS_AZURE_STORAGE_ACCOUNT}\""                                 >> ${ENVVARS}
 echo ""                                                                                                  >> ${ENVVARS}
 echo "##########################################################################################"        >> ${ENVVARS}
