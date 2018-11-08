ba39d5cc8bbac616b92b2ed17ce64cd959131b6a
Parvez <ppathan@users.noreply.github.com>
2018-11-08 12:37:32 -0800

    Merging the hot-fix

--- solutions/remotemonitoring/single-vm/setup.sh
+++ solutions/remotemonitoring/single-vm/setup.sh
@@ -66,6 +66,36 @@ export APPLICATION_SECRET=$PCS_APPLICATION_SECRET
 
 # ========================================================================
 
+### Install Docker
+
+install_docker_ce() {
+    apt-get update
+    # Remove old packages if installed
+    set +e
+    apt-get remove docker docker-engine docker.io
+    set -e
+    # Install Docker's GPG key
+    apt-get -y --force-yes --no-install-recommends install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    local host_name=$1
+    if (echo $host_name | grep -c  "\.cn$") ; then
+        # If the host name has .cn suffix, dockerhub in China will be used to avoid slow network traffic failure.
+        DOCKER_DOWNLOAD_URL="https://mirror.azure.cn/docker-ce/linux/"
+    else
+        DOCKER_DOWNLOAD_URL="https://download.docker.com/linux/"
+    fi
+
+    curl -fsSL $DOCKER_DOWNLOAD_URL$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+    # Add Docker repository to get up to date packages
+    add-apt-repository "deb [arch=amd64] $DOCKER_DOWNLOAD_URL$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+    apt-get update
+    # Install
+    apt-get -y install docker-ce docker-compose
+    # Test
+    docker run --rm hello-world && docker rmi hello-world
+}
+
+install_docker_ce $HOST_NAME $5
+
 # Configure Docker registry based on host name
 # ToDo: we may need to add similar parameter to AzureGermanCloud and AzureUSGovernment
 config_for_azure_china() {
