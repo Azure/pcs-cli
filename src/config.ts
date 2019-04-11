9d4cd1a1c602f240ac5847cc21ff7691647b24a9
Avni Patel <apatel@microsoft.com>
2019-04-11 16:20:05 -0700

    merge

--- src/config.ts
+++ src/config.ts
@@ -1,48 +1,10 @@
 export class Config {
-    public AADTenantId: string;
-    public AADLoginURL: string;
-    public AuthIssuerURL: string;
+    public KeyVaultName: string;
     public ApplicationId: string;
     public ServicePrincipalSecret: string;
-    public AzureStorageAccountKey: string;
-    public AzureStorageAccountName: string;
-    public AzureStorageEndpointSuffix: string;
-    public AzureStorageConnectionString: string;
-    public AzureActiveDirectoryEndpointUrl: string;
-    public AzureResourceManagerEndpointUrl: string;
-    public AzureMapsKey: string;
-    public CloudType: string;
-    public SubscriptionId: string;
-    public SolutionName: string;
-    public IotHubName: string;
-    public DeploymentId: string;
-    public AppInsightsInstrumentationKey: string;
-    public DocumentDBConnectionString: string;
     public DockerTag: string;
     public DNS: string;
-    public EventHubEndpoint: string;
-    public EventHubName: string;
-    public EventHubPartitions: string;
-    public IoTHubConnectionString: string;
     public LoadBalancerIP: string;
     public Runtime: string;
-    public SolutionType: string;
     public TLS: {cert: string, key: string, fingerPrint: string};
-    public WebUIConfig: { 
-        authEnabled: boolean,
-        authType: string,
-        aad: {
-            tenant: string,
-            appId: string
-        }
-    };
-    public MessagesEventHubConnectionString: string;
-    public MessagesEventHubName: string;
-    public ActionsEventHubConnectionString: string;
-    public ActionsEventHubName: string;
-    public TelemetryStorgeType: string;
-    public TSIDataAccessFQDN: string;
-    public Office365ConnectionUrl: string;
-    public LogicAppEndpointUrl: string;
-    public SolutionWebsiteUrl: string;
 }
