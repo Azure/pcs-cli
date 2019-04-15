38d72585a99de088d15a5e4c2f90d064e14cf273
Elvin Morales <elmoral@microsoft.com>
2019-04-15 13:35:00 -0700

    CLI release for RM 3.0.0

--- src/index.ts
+++ src/index.ts
@@ -357,7 +357,7 @@ function main() {
                         } else {
                             // For a released version the docker tag and version should be same
                             // Default to latest released verion (different for remotemonitoring and devicesimulation)
-                            const version = (program.type === 'remotemonitoring') ? '2.2.0' : 'DS-2.0.2';
+                            const version = (program.type === 'remotemonitoring') ? '3.0.0' : 'DS-2.0.2';
                             answers.version = version;
                             answers.dockerTag = version;
                         }
